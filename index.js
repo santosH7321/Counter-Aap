4cb252ed858c3b9b4054da48cdc8fc7f769edef8
SANTOSH KUMAR <116185780+santosH7321@users.noreply.github.com>
2023-06-28 15:47:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const countValue = document.querySelector('#counter');
+
+const increment = () =>{
+    // get the vlue from UI
+    let value = parseInt(countValue.innerText);
+    //Update the value
+    value = value + 1;
+    //set the value onto UI
+    countValue.innerText = value;
+};
+
+
+const decrement = () =>{
+        // get the vlue from UI
+        let value = parseInt(countValue.innerText);
+        //Update the value
+        value = value - 1;
+        //set the value onto UI
+        countValue.innerText = value;
+};
